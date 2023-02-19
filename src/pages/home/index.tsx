0cf4fc22f1ff8c1b3e0ca25d0fd16e233ed1c3ac
ansxy <ansarfadillah17@gmail.com>
2023-02-20 00:55:45 +0800

    refactoring : put session on navbar

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,28 @@
-import { SessionProvider } from "next-auth/react";
-import React from "react";
+import { SessionProvider, signOut } from "next-auth/react";
 import UserProvider from "../../context/user/UserProvider";
+import React from "react";
+import Splash from "./components/splash.home";
+import { useState } from "react";
 
 const Index: React.FC<any> = () => {
+  const [isOpen, setIsOpen] = useState<Boolean | null>(false);
   return (
-    <SessionProvider>
-      <UserProvider>
-        <></>
-      </UserProvider>
-    </SessionProvider>
+    <>
+      <div className="w-full flex flex-col items-center justify-center">
+        <section className="h-96 w-full bg-gradient-to-b from-blue-500 to-blue-300 relative">
+          {/* <Splash /> */}
+        </section>
+        <section className="w-[55%] relative" onClick={() => setIsOpen(true)}>
+          <div className="w-full h-[25rem] bg-white absolute -top-14 z-40 drop-shadow-lg rounded-md"></div>
+        </section>
+      </div>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50"
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
+    </>
   );
 };
 
