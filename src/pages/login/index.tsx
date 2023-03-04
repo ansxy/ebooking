c289c87ae88c92156c0ef686526254181b7abbad
ansxy <ansarfadillah17@gmail.com>
2023-03-05 02:39:14 +0800

    feature : login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
+import { FormEventHandler, useState, useContext } from "react";
+import { UserContext, UserContextProps } from "../../context/user/UserContext";
+import { getToken } from "next-auth/jwt";
 
-export const Index: React.FC<any> = () => {
-  return <></>;
+const SignIn: NextPage = (props): JSX.Element => {
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const { login } = useContext<UserContextProps>(UserContext);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await login(userInfo);
+  };
+  return (
+    <div className="sign-in-form">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input
+          value={userInfo.email}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, email: target.value })
+          }
+          type="email"
+          placeholder="john@email.com"
+        />
+        <input
+          value={userInfo.password}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, password: target.value })
+          }
+          type="password"
+          placeholder="****"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
+
+export default SignIn;
