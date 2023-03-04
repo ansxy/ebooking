e44b72e0d8c4a148040564a0e45332f1f4a53e2d
ansxy <ansarfadillah17@gmail.com>
2023-03-05 02:38:26 +0800

    refactoring : session and jwt

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 import NextAuth from "next-auth";
 import bycrpt from "bcrypt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../../../lib/prisma";
-import { decode, encode } from "next-auth/jwt";
-import async from "./signin";
+import jwt from "jsonwebtoken";
 export default NextAuth({
   providers: [
     CredentialsProvider({
@@ -20,7 +20,6 @@ export default NextAuth({
             },
           });
           if (!user) {
-            console.log("usertidak ada");
             return null;
           }
           const valid = await bycrpt.compare(
@@ -31,28 +30,119 @@ export default NextAuth({
             return null;
           }
           if (user) {
-            return { ...user };
+            const secret = process.env.NEXTAUTH_SECRET;
+            const accessToken = jwt.sign(
+              {
+                user: {
+                  role: user.role,
+                  id: user.id,
+                  email: user.email,
+                },
+                exp: Math.floor(Date.now() / 1000) + 60 * 60,
+              },
+              secret as string
+            );
+            return {
+              accessToken: accessToken,
+              email: user.email,
+              role: user.role,
+              name: user.name,
+              id: user.id,
+            };
           }
+
           return null;
         } catch (error) {
-          console.log(error);
+          return error;
         }
       },
     }),
   ],
   pages: {
-    signIn: "/auth/sigin",
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 10000,
+    // updateAge: 0,
   },
   callbacks: {
-    async jwt({ token, account, profile }) {
-      console.log(token, account);
-      if (account) {
-        token.accessToken = account.access_token;
+    async jwt({ token, account, user, profile }) {
+      if (account && user) {
+        token.accessToken = user.accessToken;
+        token.role = user?.role;
       }
       return token;
     },
   },
-  jwt: {
-    maxAge: 60 * 60 * 24 * 30,
-  },
 });
+
+// import { NextAuthOptions } from "next-auth";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import axios from "axios";
+// import NextAuth from "next-auth/next";
+// type NextAuthOptionsCallback = (
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) => NextAuthOptions;
+
+// const nextAuthOptions: NextAuthOptionsCallback = (req, res) => {
+//   return {
+//     providers: [
+//       CredentialsProvider({
+//         credentials: {
+//           email: { label: "Email", type: "text" },
+//           password: { label: "Password", type: "password" },
+//         },
+//         async authorize(credentials) {
+//           try {
+//             const response = await axios.post(
+//               "http://localhost:3000/api/auth/singin",
+//               {
+//                 email: credentials?.email,
+//                 password: credentials?.password,
+//               }
+//             );
+//             const cookies = response.headers["Set-cookie"];
+//             console.log(cookies);
+//             res.setHeader("Set-Cookie", cookies);
+//             return {
+//               email: response.data.user.email,
+//               role: response.data.user.role,
+//               name: response.data.user.name,
+//               id: response.data.user.id,
+//             };
+//           } catch (error) {
+//             console.log(error);
+//           }
+//         },
+//       }),
+//     ],
+//     pages: {
+//       signIn: "/login",
+//     },
+//     jwt: {
+//       maxAge: 60 * 60 * 24 * 30,
+//       secret: process.env.NEXTAUTH_SECRET,
+//     },
+//     callbacks: {
+//       async jwt({ token, account, user, profile }) {
+//         if (account) {
+//           token.accessToken = account.access_token;
+//           token.role = user?.role;
+//         }
+//         return token;
+//       },
+//       async session({ session, user, token }) {
+//         session.accessToken = token.accessToken;
+//         session.user.id = token.sub;
+//         return session;
+//       },
+//     },
+//   };
+// };
+// export default (req: NextApiRequest, res: NextApiResponse) => {
+//   return NextAuth(req, res, nextAuthOptions(req, res));
+// };
