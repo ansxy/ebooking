3b4e297a02afb9b0077c4d6b6c5d40046e55ce73
ansxy <ansarfadillah17@gmail.com>
2023-03-17 02:39:43 +0800

    fix login and register api

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,31 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 import bycrpt from "bcrypt";
-import { getSession } from "next-auth/react";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { name, email, password } = req.body;
+  const { name, email, password, role } = req.body;
 
   const hashedPassword = await bycrpt.hash(password, 10);
   try {
     const user = await prisma.user.create({
       data: {
-        name,
-        email,
+        name: name,
+        email: email,
+        role: role,
         password: hashedPassword,
       },
     });
-    const session = await getSession({ req });
-    if (session) {
-      return res.status(200).json({ user });
-    } else {
-      return res.status(200).json({ message: "Success" });
-    }
+    return res.status(200).json({ message: "Success", data: user });
   } catch (error) {
-    console.error(error);
     return res
       .status(500)
       .json({ error: "An error occurred while creating your account" });
